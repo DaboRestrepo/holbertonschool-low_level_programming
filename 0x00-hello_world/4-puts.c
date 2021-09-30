828a3002fc996d32be86123055f34b7739614380
Daniela Botero <3786@hotlbertonschool.com>
2021-09-30 14:16:38 -0700

    Imprimir con puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/*Se trata de imprimir la frase con el uso de puts*/
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
