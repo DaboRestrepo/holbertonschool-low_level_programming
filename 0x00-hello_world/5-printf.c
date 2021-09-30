32fb5779957fb5a19ef61957beb60fa847cda83b
Daniela Botero <3786@hotlbertonschool.com>
2021-09-30 15:08:20 -0700

    Imprimir con printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* Descripción: uso de prinf para imprimir  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
- return(0);  
+return (0);
 }
