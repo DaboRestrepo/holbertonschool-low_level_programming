fca897e67cdfbce5a2c4b7cac450f927508c76d1
Daniela Botero <3786@hotlbertonschool.com>
2021-10-01 15:51:26 -0700

    Número menores de 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+char n;
+for (n = 0; n < 10; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
