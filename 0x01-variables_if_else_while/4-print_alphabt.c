55f18e32a8575a671a07b95721aca421417aa0d9
Daniela Botero <3786@hotlbertonschool.com>
2021-10-01 15:37:23 -0700

    sin q y sin e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,15 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 char n;
 n = 97;
-for (n = 97; n <= 123; n++)
+for (n = 97; n <= 122; n++)
+{
 if (n != 113 && n != 101)
 putchar(n);
+}
 putchar(10);
 return (0);
 }
