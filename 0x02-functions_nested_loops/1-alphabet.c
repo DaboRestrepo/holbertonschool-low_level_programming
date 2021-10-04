54467e5f2e1577cdd0169be13ac47ea1a06f05e4
Daniela Botero <3786@hotlbertonschool.com>
2021-10-04 14:15:07 -0700

    Retornar alfabeto

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - imprimir el alfabeto.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char h[] = {"abcdefghijklmnopqrstuvwxyz"};
+int a;
+
+for (a = 0 ; a <= 26 ; a++)
+
+_putchar(h[a]);
+_putchar('\n');
+}
