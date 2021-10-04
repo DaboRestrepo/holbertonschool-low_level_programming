d0c04ae853eaf134c26b1c148df22b93795afada
Daniela Botero <3786@hotlbertonschool.com>
2021-10-04 12:56:23 -0700

    Imprimir _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+int a = 95;
+int b = 112;
+int c = 117;
+int d = 116;
+int e = 99;
+int f = 104;
+int g = 97;
+int h = 114;
+    
+putchar(a);
+putchar(b);
+putchar(c);
+putchar(d);
+putchar(e);
+putchar(f);
+putchar(g);
+putchar(h);
+putchar('\n');
+return(0);
+}
