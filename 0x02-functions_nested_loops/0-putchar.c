05c263ac4d29becb9fcc0b83d5aa57fe9efa81b4
Daniela Botero <3786@hotlbertonschool.com>
2021-10-04 13:26:23 -0700

    Imprimir putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,25 +7,15 @@
 
 #include <stdio.h>
 
+#include <stdio.h>
+
 int main(void)
 {
-int a = 95;
-int b = 112;
-int c = 117;
-int d = 116;
-int e = 99;
-int f = 104;
-int g = 97;
-int h = 114;
-    
-putchar(a);
-putchar(b);
-putchar(c);
-putchar(d);
-putchar(e);
-putchar(f);
-putchar(g);
-putchar(h);
+char h[8] = {"_putchar"};
+int a;
+
+for (a = 0 ; a <= 8 ; a++)
+putchar(h[a]);
 putchar('\n');
 return(0);
 }
