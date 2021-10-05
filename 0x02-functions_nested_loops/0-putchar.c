428ec0eead2ae2351ed346e076fb8d6d5b376f0e
Daniela Botero <3786@hotlbertonschool.com>
2021-10-05 10:14:05 -0700

    escribir putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,9 @@
  * Return: Always 0.
  */
 
-#include <stdio.h>
-
-#include <stdio.h>
-
 int main(void)
 {
-char h[8] = {"_putchar"};
+char h[] = {"_putchar"};
 int a;
 
 for (a = 0 ; a <= 8 ; a++)
