53f01e3dad029a84bbb3cd1f71cbcb9574e5f60f
Daniela Botero <3786@hotlbertonschool.com>
2021-10-04 19:28:26 -0700

    Horas del día

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - Imprimir las horas
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+int m, n;
+for (m = 0; m < 24; m++)
+{
+for (n = 0; n < 24; n++)
+{
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar(':');
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+_putchar('\n');
+}
+}
+}
