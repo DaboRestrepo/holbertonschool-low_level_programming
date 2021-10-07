f65c4777c678fb13faacad87f786e3a764b0c7c5
Daniela Botero <3786@hotlbertonschool.com>
2021-10-07 08:25:19 -0700

    Imprimir los números del 1 al 9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - función que multiplica dos dígitos
+ *
+ * @a: primer dígito.
+ * @b: segundo dígito.
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
