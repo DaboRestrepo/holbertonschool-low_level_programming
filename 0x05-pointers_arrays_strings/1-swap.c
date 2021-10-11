2c0b244e7b76553769bc7aa19eb4040577312ff5
Daniela Botero <3786@hotlbertonschool.com>
2021-10-11 14:23:05 -0700

    Intercambiar los valores de a y b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - intercambiar el valor de a y b.
+ * @a: valor a intercambiar por b
+ * @b: valor a intercambiar por a
+ * Return: void.
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
