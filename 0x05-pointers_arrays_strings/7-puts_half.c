ef0962a33015ee4944d7b5f98c1478d7437a758d
Daniela Botero <3786@hotlbertonschool.com>
2021-10-12 12:00:39 -0700

    Imprimir la mitad de los caracteres

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - imprimir la mitad de los caracteres
+ * @str: copia del string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int c = 0;
+int p;
+int c2;
+
+while (*(str + c))
+{
+c++;
+}
+
+if (c % 2 != 0)
+{
+c2 = (c - 1) / 2;
+}
+else
+{
+c2 = c / 2;
+}
+for (p = c2; p < c; p++)
+{
+_putchar(str[p]);
+}
+_putchar('\n');
+}
