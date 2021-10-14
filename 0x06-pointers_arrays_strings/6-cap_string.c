a34a4da189f4c1a99fed547e34560b04801a82ed
Daniela Botero <3786@hotlbertonschool.com>
2021-10-14 16:52:03 -0700

    Inicial mayúscula

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,21 @@
 #include "main.h"
-
-
 /**
  * cap_string - Cambiar a mayúscula.
  * @tmp: puntero temporal.
  * Return: tmp
  */
-
 char *cap_string(char *tmp)
 {
-int pos = 0;
-int pos_cond;
-char cond[]= {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', 32, 9, 10};
-int count = 13;
-
+int pos = 0, count = 13, pos_cond;
+char cond[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', 32, 9, 10};
 
 while (tmp[pos])
 {
 pos_cond = 0;
 while (pos_cond < count)
 {
-if ((pos == 0 || tmp[pos - 1] == cond[pos_cond]) && (tmp[pos] >= 97 && tmp[pos] <= 122))
+if ((pos == 0 || tmp[pos - 1] == cond[pos_cond])
+&& (tmp[pos] >= 97 && tmp[pos] <= 122))
 {
 tmp[pos] = tmp[pos] - 32;
 }
