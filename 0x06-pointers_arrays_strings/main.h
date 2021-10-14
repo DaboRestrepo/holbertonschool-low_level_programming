c3582bc1bd5b591ce1655c00d3638e04e28958e6
Daniela Botero <3786@hotlbertonschool.com>
2021-10-14 16:47:21 -0700

    Inicial mayúscula

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ int _strcmp(char *s1, char *s2);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *tmp);
+char *cap_string(char *tmp);
 char *leet(char *tmp);
 
 #endif
