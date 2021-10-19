9f7d2b375c4323b6cf07b3c75dab8c1953c30afc
Daniela Botero <3786@hotlbertonschool.com>
2021-10-19 16:47:12 -0700

    Encontrar c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+
 /**
  * _strchr - Imprimir el string desde c
  * @s: Puntero del string
@@ -7,17 +9,24 @@
  */
 
 
+
 char *_strchr(char *s, char c)
 {
 int pos = 0;
+int count = 0;
 
-for (pos = 0; s[pos]; pos++)
+while (s[count] != '\0')
+{
+count++;
+}
+count++;
+
+for (pos = 0; pos <= count; pos++)
 {
 if (s[pos] == c)
 {
 return (s + pos);
-break;
 }
 }
-return ('\0');
+return (NULL);
 }
