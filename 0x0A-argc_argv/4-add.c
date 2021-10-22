a527d50e8c24f153ae67612363b5541d7c781ec9
Daniela Botero <3786@hotlbertonschool.com>
2021-10-22 15:48:28 -0700

    Sumar los dígitos

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,15 +13,9 @@ int main(int argc, char **argv)
 {
 int count, i = 0, sum = 0;
 
-if (argc < 2)
-{
-printf("0\n");
-}
-else
-{
 for (count = 1; count < argc; count++)
 {
-for (i = 0; i < count; i++)
+for (i = 0; argv[count][i] != '\0'; i++)
 {
 if (isdigit(argv[count][i]) == 0)
 {
@@ -32,7 +26,6 @@ return (1);
 sum += atoi(argv[count]);
 }
 printf("%d\n", sum);
-}
 return (0);
 }
 
