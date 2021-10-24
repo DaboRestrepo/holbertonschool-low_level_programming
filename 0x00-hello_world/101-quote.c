00e18a41918cb03669107e219d8ae80f814a4f00
Daniela Botero <3786@hotlbertonschool.com>
2021-10-24 10:52:54 -0700

    Imprimir frase

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-int main (void)
+/**
+ * main - Imprime la frase sin usar printf ni puts
+ * Return: always 1.
+ */
+int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
