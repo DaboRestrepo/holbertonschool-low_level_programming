195b7babfc3fa4c4abeaf45522c75fc616477821
Daniela Botero <3786@hotlbertonschool.com>
2021-10-28 18:52:21 -0700

    Concatenar con n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,15 +2,30 @@
 #include "main.h"
 
 /**
- *
- *
+ * _strlen - cuenta los char de str
+ * @s: toma la posición de str
+ * Return: la cuenta
+ */
+
+unsigned int _strlen(char *s)
+{
+unsigned int count = 0;
+while (*(s + count))
+count++;
+return (count);
+}
+
+/**
+ * string_nconcat - concatenar con n
+ * @s1: Destino
+ * @s2: fuente
+ * @n: tamaño
+ * Return: ptr.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *string;
-unsigned int len_s1 = 0, len_s2 = 0;
 unsigned int m, o;
-
 if (s1 == NULL)
 {
 s1 = "";
@@ -19,28 +34,16 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-
-while (s1[len_s1])
-{
-len_s1++;
-}
-while (s2[len_s2])
-{
-len_s2++;
-}
-
-string = malloc(sizeof(char) * ((len_s1 + n) + 1));
+string = malloc(sizeof(char) * ((_strlen(s1) + n) + 1));
 if (string == NULL)
 {
 return (NULL);
 }
-
 for (m = 0; s1[m] != '\0'; m++)
 {
 string[m] = s1[m];
 }
-
-if (n >= len_s2)
+if (n >= _strlen(s2))
 {
 for (o = 0; s2[o] != '\0'; o++)
 {
