dab126be380965b3bd764edf9bd951f8b2205c19
Daniela Botero <3786@hotlbertonschool.com>
2021-10-28 17:26:54 -0700

    Alocar espacio para cualquier array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *
- *
- *
+ * _calloc - Alocar memoria para cualquier array.
+ * @nmemb: Tamaño del type.
+ * @size: Cantidad de type.
+ * Return: Null si algún argumento es = 0.
+ * Null si _calloc falla y ptr si es exitoso.
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *ptr;
-unsigned i;
+unsigned int i;
 
 if (nmemb == 0 || size == 0)
 {
