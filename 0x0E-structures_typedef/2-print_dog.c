d5d66cd0f3b3fd9a0615bf51578881e55775e503
Daniela Botero <3786@hotlbertonschool.com>
2021-11-01 12:27:14 -0700

    Print struct dog with main statments

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,22 @@
 
 void print_dog(struct dog *d)
 {
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+}
+else if (d->age == '\0')
+{
+printf("Age: (nil)\n");
+}
+else if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
 printf("Name: %s\n", d->name);
 printf("Age: %6f\n", d->age);
 printf("Owner: %s\n", d->owner);
+}
 }
