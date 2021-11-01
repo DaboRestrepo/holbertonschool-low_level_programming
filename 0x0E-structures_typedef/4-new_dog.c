383686fea9b5696d6a55376df26473cdf2978ccb
Daniela Botero <3786@hotlbertonschool.com>
2021-11-01 14:57:44 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,17 +9,55 @@
  *
  * Return: Null if fail and d if success
  */
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
 
 d = malloc(sizeof(dog_t));
-if (d == NULL)
-return (NULL);
 
-d->name = name;
+if (d)
+{
+d->name = malloc(sizeof(char) * _strlen(name));
+if (d->name)
+{
+d->owner = malloc(sizeof(char) * _strlen(owner));
+_strcpy(d->name, name);
+_strcpy(d->owner, owner);
 d->age = age;
-d->owner = owner;
+}
+else
+{
+free (d->name);
+free (d);
+}
+}
 
 return (d);
 }
+
+int _strlen(char *s)
+{
+int count = 0;
+
+while (s[count])
+{
+count++;
+}
+return (count);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+int index;
+
+for (index = 0; src[index]; index++)
+{
+dest[index] = src[index];
+}
+return (dest);
+}
+
