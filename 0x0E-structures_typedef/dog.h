521b5ce779c9fcad5f8a9e77fc9d2c8b06a3f3f8
Daniela Botero <3786@hotlbertonschool.com>
2021-11-01 12:52:31 -0700

    Change the name to the type dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,10 +11,14 @@
  */
 struct dog
 {
+if (name == NULL)
+{
 char *name;
 float age;
 char *owner;
-};
+}
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
