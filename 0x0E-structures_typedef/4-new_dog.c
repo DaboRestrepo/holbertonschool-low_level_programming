1a82ebd32e9f6ac2fd2f4d5efb49eb35184372d8
Daniela Botero <3786@hotlbertonschool.com>
2021-11-01 15:35:52 -0700

    Create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -55,14 +55,15 @@ else
 d->name = malloc(sizeof(char) * (_strlen(name) + 1));
 if (d->name == NULL)
 {
-free(d->name);
+free(d);
 return (NULL);
 }
 
 d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 if (d->owner == NULL)
 {
-free(d->owner);
+free(d->name);
+free(d);
 return(NULL);
 }
 
