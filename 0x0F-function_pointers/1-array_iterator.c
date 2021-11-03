a2c2eb930f95971d6e524153245bb524d603b267
Daniela Botero <3786@hotlbertonschool.com>
2021-11-03 09:34:07 -0700

    Do the actions of the function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - interate with the actions of the
+ * functions.
+ * @array: elements to interate.
+ * @size: size of the array.
+ * @action: todo things.
+ * Return: void.
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i = 0;
+
+if (array)
+{
+for (; i < size; i++)
+{
+action(array[i]);
+}
+}
+else
+return;
+}
+
