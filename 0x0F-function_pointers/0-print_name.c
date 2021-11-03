525905e55f2150adcdea083b648a4661ddff09f8
Daniela Botero <3786@hotlbertonschool.com>
2021-11-03 09:05:23 -0700

    Print name with the values of main

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,8 +3,10 @@
 #include "function_pointers.h"
 
 /**
- *
- *
+ * print_name - Print the name giving by main
+ * @name: first value type char
+ * @f: second value type char
+ * Return: void.
  */
 
 void print_name(char *name, void (*f)(char *))
