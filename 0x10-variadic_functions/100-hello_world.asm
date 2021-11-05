0353be4b63a76ebb966e2708ccaa0037f67eade7
Daniela Botero <3786@hotlbertonschool.com>
2021-11-05 09:34:17 -0700

    Assembly print

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -6,13 +6,12 @@ main:
 mov rax, 1
 mov rdi, 1
 mov rsi, msg
-mov rdx, msglen
+mov rdx, 17
 syscall
 
 mov rax, 60
-mov rdi, 0
+mov rdi, rdi
 syscall
 
 section .mens
 msg: db "Hello, world", 10
-msglen: equ $ - msg
