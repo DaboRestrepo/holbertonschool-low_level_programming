486cd62f0e8e6645ad2abfd3c8cf90757de15420
DaboRestrepo <daborestrepo@gmail.com>
2021-11-11 17:44:51 -0500

    Size of the list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 /**
- *
- *
+ * list_len - Give the number of elements in the list.
+ * @h: The elements of the list.
+ * Return: the count.
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while(h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-    return (count);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
