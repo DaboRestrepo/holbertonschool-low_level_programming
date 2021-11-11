dd816ea527ad1e40c6435b0337a08b5a392bf269
DaboRestrepo <daborestrepo@gmail.com>
2021-11-11 17:31:13 -0500

    Print a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 
 /**
- *
- *
+ * print_list - Print a linked list
+ * @h: List
+ * Return: the size of the list
  */
 
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        count++;
-        h = h->next;
-    }
-    return (count);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
