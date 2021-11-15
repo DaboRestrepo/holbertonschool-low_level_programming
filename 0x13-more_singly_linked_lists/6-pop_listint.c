5488a0fe9b36c44df35787c4a95ad221cf26f77a
DaboRestrepo <daborestrepo@gmail.com>
2021-11-15 16:32:00 -0500

    Free the head and return the erased value

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * pop_listint - Free the memory of the head.
+ * @head: The list.
+ * Return: The element that was erased.
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n = (*head)->n;
+	listint_t *temp = NULL;
+
+
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (n);
+}
