e27fa9224ab980a7c8bd51a6425e587fdd0f8caf
DaboRestrepo <daborestrepo@gmail.com>
2021-11-15 16:13:31 -0500

    Free memory and let head equal to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - Free the memory of the list and
+ * let head equal to NULL.
+ * @head: The list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = NULL, *temp2 = NULL;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		temp2 = temp;
+		temp = temp->next;
+		free(temp2);
+	}
+	*head = NULL;
+}
