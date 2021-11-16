67d3eb018b8b5338a627964a40e17380b7b22d3c
DaboRestrepo <daborestrepo@gmail.com>
2021-11-16 09:41:44 -0500

    Fix the conditions

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		printf("(nil)");
-	}
-
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		if (h->n == '\0')
+			printf("(nil)");
+		else
+			printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
