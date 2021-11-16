240e10320c8c74544db1882d929ae4e16d5e46df
DaboRestrepo <daborestrepo@gmail.com>
2021-11-16 12:43:53 -0500

    Fix the first if

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *temp = *head, *temp2;
 
-	if (head == NULL)
+	if (temp == NULL)
 		return (-1);
 
 	if (index == 0)
