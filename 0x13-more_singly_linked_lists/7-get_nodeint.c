61621a02249b40136f4e11c26557b83517ebe32b
DaboRestrepo <daborestrepo@gmail.com>
2021-11-16 11:08:29 -0500

    Fix the condition of if

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
 	for (i = 0; head->next != NULL && i < index; i++)
 	{
-		if (head == NULL)
-			return (NULL);
-
 		head = head->next;
 	}
 	return (head);
