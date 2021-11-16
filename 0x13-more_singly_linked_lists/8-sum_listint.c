3f025b3d9c56e8b8f9eb77a4fb1f8536a7a113f7
DaboRestrepo <daborestrepo@gmail.com>
2021-11-15 19:35:32 -0500

    Add the elements of the list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_listint - Add the integers of the list.
+ * @head: The list.
+ * Return: The sum.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
