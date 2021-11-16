7b24ee31b24136d576029aeecb326d4c266bea9d
DaboRestrepo <daborestrepo@gmail.com>
2021-11-16 12:39:10 -0500

    Fix the value of temp2

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *temp = *head, *temp2 = NULL;
+	listint_t *temp = *head, *temp2;
 
 	if (head == NULL)
 		return (-1);
