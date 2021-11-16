5c162f28b4e6023728c52648ce63ff77e5b10df4
DaboRestrepo <daborestrepo@gmail.com>
2021-11-16 12:35:27 -0500

    Delete a node in a giving index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - Delete a node in the index position.
+ * @head: The list.
+ * @index: Integer that pass for the list.
+ * Return: -1 if fail, 1 if success.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp = *head, *temp2 = NULL;
+
+	if (head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	for (i = 0; i < index - 1; i++)
+	{
+		if (temp->next == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+	temp2 = temp->next;
+	temp->next = temp2->next;
+	free(temp2);
+	return (1);
+}
