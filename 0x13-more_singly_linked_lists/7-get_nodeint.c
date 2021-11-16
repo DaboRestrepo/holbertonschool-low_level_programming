6f5a1c332f929291c0061859b71001d0cc111fc7
DaboRestrepo <daborestrepo@gmail.com>
2021-11-16 10:45:07 -0500

    Add a node in the index position

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-	{
-		return (0);
-	}
 	for (i = 0; head->next != NULL; i++)
 	{
 		if (i == index)
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,24 +1,34 @@
 #include "lists.h"
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+/**
+ * insert_nodeint_at_index - Put a node in the element in the index position.
+ * @head: The list.
+ * @idx: Integer that pass for the list.
+ * @n: data of the node.
+ * Return: The address of the new node.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head,
+unsigned int idx, int n)
 {
-    listint_t /**temp = NULL,*/ *new = NULL, *h = NULL;
-    unsigned int i = 0;
+	listint_t *new = NULL, *h = NULL;
+	unsigned int i = 1;
 
-    h = *head;
+	h = *head;
+	new = malloc(sizeof(listint_t));
+		if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 1)
+	{
+		new->next = h;
+		h = new;
+		return (&*new);
+	}
 
-    for (; h->next != NULL; i++)            /* search the correct place */
-    {
-        if (i == idx)
-        {
-            new = malloc(sizeof(listint_t)); /* Get the new node */
-            if (new == NULL)
-                return (NULL);
-            new->n = n;
-            new->next = h->next;
-            h->next = new;
-        }
-        else
-            return (NULL);
-    }
-    return (&*new);
+	for (; h->next != NULL && i < idx - 1; i++)
+	{
+		h = h->next;
+	}
+	new->next = h->next;
+	h->next = new;
+	return (&*new);
 }
