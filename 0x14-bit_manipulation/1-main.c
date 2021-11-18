7123cda22a8408666e3149d499f734874a909875
DaboRestrepo <daborestrepo@gmail.com>
2021-11-18 18:00:33 -0500

    Use of recursive way

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -14,7 +14,11 @@ int main(void)
     printf("\n");
     print_binary(98);
     printf("\n");
-    print_binary(-1024);
+    print_binary(1024);
+    printf("\n");
+    print_binary(9);
+    printf("\n");
+    print_binary(8);
     printf("\n");
     print_binary((1 << 10) + 1);
     printf("\n");
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,17 +6,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n > 1)
 	{
-		_putchar('0');
-		return;
+		print_binary(n >> 1);
 	}
-	while (n > 0)
-	{
-		if ((n & 1) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		n >>= 1;
-	}
+	_putchar((n & 1) + '0');
 }
