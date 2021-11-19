20ac4388356acb657f14dfa4f69cb7ace2126a6f
DaboRestrepo <daborestrepo@gmail.com>
2021-11-18 19:34:31 -0500

    flip the bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * flip_bits - count the bits that has to flip
+ * to be equals.
+ * @n: first number.
+ * @m: second number.
+ * Return: the count.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0, bin_n, bin_m;
+
+	while (n > 0 && m > 0)
+	{
+		bin_n = (n & 1);
+		bin_m = (m & 1);
+		n = n >> 1;
+		m = m >> 1;
+		if (bin_m != bin_n)
+			count++;
+	}
+	return (count);
+}
