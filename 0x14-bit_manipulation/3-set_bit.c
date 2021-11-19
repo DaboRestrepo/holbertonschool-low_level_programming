92b0c165e171d3aa0b8c48b7043639dfc0123501
DaboRestrepo <daborestrepo@gmail.com>
2021-11-18 19:04:12 -0500

    Fix Betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,9 +1,15 @@
 #include "main.h"
+/**
+ * set_bit - change a bit in index position.
+ * @n: pointer to the number.
+ * @index: position.
+ * Return: 1 if success or -1 if fails.
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > 63)
-        return (-1);
+	if (index > 63)
+		return (-1);
 
-    *n ^= (-1 ^ *n) & (1 << index);
-    return (1);
+	*n ^= (-1 ^ *n) & (1 << index);
+	return (1);
 }
