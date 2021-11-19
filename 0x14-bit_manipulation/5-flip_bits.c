792dd7a23c4b8f69af4d7eedea35137bbbfb16f2
DaboRestrepo <daborestrepo@gmail.com>
2021-11-18 21:26:59 -0500

    Fix the logical operator of if

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,14 +10,14 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0, bin_n, bin_m;
 
-	while (n > 0 && m > 0)
+	while (n > 0 || m > 0)
 	{
 		bin_n = (n & 1);
 		bin_m = (m & 1);
-		n = n >> 1;
-		m = m >> 1;
 		if (bin_m != bin_n)
 			count++;
+		n = n >> 1;
+		m = m >> 1;
 	}
 	return (count);
 }
