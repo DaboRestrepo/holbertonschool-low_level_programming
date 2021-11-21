8aad969dd338fc4b3d1e254df9f280e30c9aaa5f
DaboRestrepo <daborestrepo@gmail.com>
2021-11-21 13:30:52 -0500

    Print the combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - Print the possible combinations of
+ * two-two digits.
+ * Return: always 0.
  */
 int main(void)
 {
-int 
+	int n, m;
+
+	for (n = 0; n <= 98; n++)
+	{
+		for (m = 0; m <= 99; m++)
+		{
+			if (n < m)
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+				if (n == 98 && m == 99)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
