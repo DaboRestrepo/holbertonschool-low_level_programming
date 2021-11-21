20d05abef6542d073d715fe3f58c3dc0876c5469
DaboRestrepo <daborestrepo@gmail.com>
2021-11-21 17:01:36 -0500

    fix output

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,16 @@ int main(void)
 {
 	long int m = 0, n = 1, o = 0, count = 0;
 
-	printf("0\n1\n");
-	for (; count < 48; count++)
+	for (; count < 50; count++)
 	{
 		o = m + n;
-		printf("%ld\n", o);
+		printf("%ld", o);
+		if (count == 49)
+			break;
+		printf(", ");
 		m = n;
 		n = o;
 	}
+	printf("\n");
 	return (0);
 }
