dd35b0c4ad268b6364512b38c25656ab99128d95
DaboRestrepo <daborestrepo@gmail.com>
2021-11-23 11:07:58 -0500

    Fix the condition

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;/* File descriptor */
-	int index;
+	/*int index;*/
 	char buf[1024];
 	ssize_t num_bytes;
 
@@ -18,12 +18,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	num_bytes = read(fd, buf, letters);
+	write(fd, buf, letters);
 	close(fd);
 	if (num_bytes == 0 || letters != (size_t)num_bytes)
 		return (0);
-	for (index = 0; buf[index] != '\0'; index++)
-	{
-		putchar(buf[index]);
-	}
 	return (num_bytes);
 }
