fa6e08674c90f8bccdaccb1b02f2b0faaf4a1981
DaboRestrepo <daborestrepo@gmail.com>
2021-11-23 12:00:56 -0500

    Add conditions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1 || filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	while (text_content[len])
 		len++;
 	write(fd, text_content, len);
