d3739192eb4169f620fc002980bcece1500c83bb
DaboRestrepo <daborestrepo@gmail.com>
2022-01-06 14:00:36 -0500

    Fix Betty

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 /**
- * @brief 
- * 
+ * sum_dlistint - Add the elements in the list.
+ * @head: the head of the list.
+ * Return: the add.
  */
 int sum_dlistint(dlistint_t *head)
 {
-    unsigned int sum = 0;
+	unsigned int sum = 0;
 
-    if (head == NULL)
-        return (0);
-    while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-    return (sum);
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
