72a5ddc0255e94cc195dd3e6a3c122d3dcb98882
DaboRestrepo <daborestrepo@gmail.com>
2022-01-06 15:05:23 -0500

    Insert the node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,11 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 	new->n = n;
-	if (idx == 0)
-	{
-		new->next = NULL;
-		return (new);
-	}
+
 	for (; i < idx - 1; i++)
 	{
 		if (head->next == NULL)
