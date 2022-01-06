d72877f8d31bb0ec71b89824fe4ee457491b8151
DaboRestrepo <daborestrepo@gmail.com>
2022-01-06 14:59:46 -0500

    Fix the conditions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	for (; i < idx - 1; i++)
 	{
+		if (head->next == NULL)
+			return (NULL);
 		head = head->next;
 	}
 	new->next = head->next;
