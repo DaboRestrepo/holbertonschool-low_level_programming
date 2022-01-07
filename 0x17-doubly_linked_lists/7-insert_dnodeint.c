e7c68d5b5c0292b0c593a9373a6220c638f6a7d5
DaboRestrepo <daborestrepo@gmail.com>
2022-01-07 10:44:51 -0500

    Fix the element

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (i == idx)
 		{
 			if (head->next == NULL)
-				return (add_dnodeint_end(h, n));
+				new = add_dnodeint_end(h, n);
 			else
 			{
 				new = malloc(sizeof(dlistint_t));
