0944dd5026cf36001e25f2be0ef7bbf996a095b9
DaboRestrepo <daborestrepo@gmail.com>
2022-01-07 14:14:49 -0500

    Fiz the for loop

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,29 +10,27 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 	dlistint_t *h = *head, *tmp;
 
-	if (head!= NULL && *head != NULL)
-	{	
-		if (index == 0)
-		{
-			*head = h->next;
-			h->prev = NULL;
-			free(h);
-			return (1);
-		}
-		i = 1;
-		for (; h != NULL; i++)
-		{
-			if (i == index)
-			{
-				if (h->next == NULL)
-					return (-1);
-				tmp = h->next;
-				h->next = tmp->next;
-				free(tmp);
-			}
-			h = h->next;
-		}
+	if (head == NULL && *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = h->next;
+		h->prev = NULL;
+		free(h);
 		return (1);
 	}
-	return (-1);
+	i = 1;
+	for (; h != NULL; i++)
+	{
+		if (i == index)
+		{
+			if (h->next == NULL)
+				return (-1);
+			tmp = h->next;
+			h->next = tmp->next;
+			free(tmp);
+		}
+		h = h->next;
+	}
+	return (1);
 }
