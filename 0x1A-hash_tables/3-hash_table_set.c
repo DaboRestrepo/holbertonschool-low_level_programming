e009dac46dda601c0a03f9583bbe59e23f4d1b50
DaboRestrepo <daborestrepo@gmail.com>
2022-01-28 18:45:04 -0500

    fix Betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -52,7 +52,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			item->next = current_item;
 			current_item = item;
-			return (1); 
+			return (1);
 		}
 	}
 	return (1);
