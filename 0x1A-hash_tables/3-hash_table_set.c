d01f103b88c18a645d0c61c61b4f20871420e925
DaboRestrepo <daborestrepo@gmail.com>
2022-01-28 19:02:58 -0500

    Merge the functions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,26 +1,5 @@
 #include "hash_tables.h"
 /**
- * pair - make the pair key: Value.
- * @key: the key word.
- * @value: the storage value.
- * Return: a new node.
- */
-hash_node_t *pair(const char *key, const char *value)
-{
-	hash_node_t *nodo_pair = malloc(sizeof(hash_node_t));
-
-	if (nodo_pair == NULL)
-	{
-		free(nodo_pair);
-		return (NULL);
-	}
-	nodo_pair->key = strdup(key);
-	nodo_pair->value = strdup(value);
-	nodo_pair->next = NULL;
-	return (nodo_pair);
-}
-
-/**
  * hash_table_set - Insert an element to the hash table.
  * @ht: the hash table.
  * @key: the key word.
@@ -29,20 +8,25 @@ hash_node_t *pair(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *current_item = NULL, *item = NULL;
+	hash_node_t *item = malloc(sizeof(hash_node_t));
 	unsigned long int index = 0;
 
+	if (item == NULL)
+	{
+		free(item);
+		return (0);
+	}
 	if (strlen(key) == 0 || !ht || !ht->array || !ht->size)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	item = pair(key, value);
-
-	current_item = ht->array[index];
-	if (current_item == NULL)
+	item->key = strdup(key);
+	item->value = strdup(value);
+	item->next = NULL;
+	if (ht->array[index] == NULL)
 		ht->array[index] = item;
-	while (current_item)
+	while (ht->array[index])
 	{
-		if (strcmp(key, current_item->key) == 0)
+		if (strcmp(key, ht->array[index]->key) == 0)
 		{
 			strcpy(ht->array[index]->value, strdup(value));
 			free(item);
@@ -50,8 +34,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
-			item->next = current_item;
-			current_item = item;
+			item->next = ht->array[index];
+			ht->array[index] = item;
 			return (1);
 		}
 	}
