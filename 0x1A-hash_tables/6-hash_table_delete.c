9c6b52ad64bc78489fcc6c46cd39a1a731952e42
DaboRestrepo <daborestrepo@gmail.com>
2022-02-02 18:22:41 -0500

    Delete a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Delete a hash table.
+ * @ht: the hash table to be erased.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *temp = NULL;
+
+	while (index < ht->size)
+	{
+		while (ht->array[index] != NULL)
+		{
+			temp = ht->array[index]->next;
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
+			free(ht->array[index]);
+			ht->array[index] = temp;
+		}
+		index++;
+	}
+}
